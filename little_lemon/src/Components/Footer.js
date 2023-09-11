2a0227d73de37b5c3f105e10cb55a26679f822f5
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2023-09-11 14:25:21 +0500

    Delete little_lemon directory

--- little_lemon/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import flogo from "../Assets/footerlogo.png"
-export default function Footer() {
-  return (
-    <footer>
-        <img src={flogo} alt="This is Logo"/>
-    </footer>
-  )
-}
